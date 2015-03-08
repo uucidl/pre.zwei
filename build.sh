a20f617604423687f03db9c0bb8ac8f81b85a02b
Nicolas Léveillé <nicolas@uucidl.com>
2015-03-08 10:41:51 +0100

    pass location of platform SDK

--- build.sh
+++ build.sh
@@ -4,20 +4,25 @@ BUILD="${HERE}/builds"
 [ -d "${BUILD}" ] || mkdir -p "${BUILD}"
 
 
-function compile_clang()
+function compile_osx()
 {
     local cflags=()
     local cxxflags=()
+    local PLATFORM_SDK="/Applications/Xcode.app/Contents//Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/usr/include/"
+
+    cflags=("${cflags[@]}" -mmacosx-version-min=10.8)
+    cflags=("${cflags[@]}" -isystem "${PLATFORM_SDK}")
+    cxxflags=("${cxxflags[@]}" -stdlib=libc++)
 
     cflags=("${cflags[@]}" "-fno-rtti" "-fno-exceptions")
     cflags=("${cflags[@]}" "-isystem" "${HERE}"/include)
     cflags=("${cflags[@]}" "-Wall" "-Wextra" "-Werror")
     cflags=("${cflags[@]}" "-Wno-padded" "-Wno-unused-parameter" "-Wno-conversion")
     cflags=("${cflags[@]}" -g -gdwarf-3)
-    cxxflags=("${cflags[@]}" "-std=c++11")
+    cxxflags=("${cxxflags[@]}" "-std=c++11")
 
     clang++ "${cflags[@]}" "${cxxflags[@]}" "$@"
 }
 
-compile_clang "${HERE}"/src/zwei_osx.cpp -o "${BUILD}"/zwei && \
+compile_osx "${HERE}"/src/zwei_osx.cpp -o "${BUILD}"/zwei && \
     find "${BUILD}" -type f -depth 1 | xargs printf "PROGRAM\t%s\n"
