c8c5931441370503ec7f556e1fee3b5c7da1a64f
Nicolas Léveillé <nicolas@uucidl.com>
2015-03-21 15:46:28 +0100

    test script stops after each program failure

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 HERE="$(dirname ${0})"
 
-"${HERE}"/build.sh | grep '^PROGRAM' | cut -f2 | while read program ; do
-                                                     printf "running ${program}\n"
-                                                     ${program} || exit 1
-                                                 done
+"${HERE}"/build.sh | grep '^PROGRAM' | cut -f2 | \
+    while read program ; do
+        printf "running ${program}\n"
+        ${program} || exit 1
+    done
+
+if [[ $? -ne 0 ]]; then
+   exit 1
+fi
 
 ##
 # some reports
