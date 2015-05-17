bf52ddd38d98e35f866412275df068c94f3ceb18
Nicolas Léveillé <nicolas@uucidl.com>
2015-05-17 12:09:17 +0200

    split source code formatting into its own script

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env sh
 HERE="$(dirname ${0})"
-source "${HERE}"/scripts/lib/user.sh
 
-CLANG_FORMAT=${CLANG_FORMAT:-$(which clang-format)}
-[ -x "${CLANG_FORMAT}" ] || die "missing CLANG_FORMAT env variable"
+# run this script before every commit
 
-function find_all_sources() {
-    find src \( \
-                  -name "*.cpp" -o -name "*.hpp" \
-                  -name "*.cc" -o -name "*.hh" \
-                  -name "*.c" -o -name "*.h" \
-                  -o -name "*.js" \
-    \) -print
-}
-
-find_all_sources | while read f; do
-                       "${CLANG_FORMAT}" -i "${f}"
-                   done
+"${HERE}"/scripts/format-sources.sh "${HERE}"
