8c71ffa03717e5eebdba8454758523185d1d5233
Nicolas Léveillé <nicolas@uucidl.com>
2016-07-10 11:35:54 +0200

    Update format-sources script

--- scripts/format-sources.sh
+++ scripts/format-sources.sh
@@ -11,12 +11,12 @@ CLANG_FORMAT=${CLANG_FORMAT:-$(which clang-format)}
 [ -x "${CLANG_FORMAT}" ] || die "missing CLANG_FORMAT env variable"
 
 function find_all_sources() {
-    find "${TOP}"/include \( \
+    [[ -d "${TOP}/include" ]] && find "${TOP}"/include \( \
                   -name "*.hpp" \
                   -o -name "*.hh" \
                   -o -name "*.h" \
     \) -print
-    find "${TOP}"/src \( \
+    [[ -d "${TOP}/src" ]] && find "${TOP}"/src \( \
                   -name "*.cpp" -o -name "*.hpp" \
                   -o -name "*.cc" -o -name "*.hh" \
                   -o -name "*.c" -o -name "*.h" \
