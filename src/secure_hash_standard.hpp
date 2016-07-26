0e0833f175b9e90cb7b84c4eaea91a777cc25ebe
nicolas <nicolas@uucidl.com>
2016-07-26 09:54:17 +0200

    Found defect in definition of sha1

--- src/secure_hash_standard.hpp
+++ src/secure_hash_standard.hpp
@@ -3,5 +3,7 @@
 #include <cstddef>
 #include <cstdint>
 
+// TODO(nicolas): this definition is unsafe, as the size of the destination
+// isn't actually checked
 zw_internal void
 sha1(uint8_t const *bytes, size_t bytes_size, uint8_t result[20]);
