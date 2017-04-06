c8e252598c55c65310f8610e3beb40bdf176f01f
Nicolas <nicolas@uucidl.com>
2017-04-06 14:09:40 +0900

    Clean-up header

--- src/zwei_config.hpp
+++ src/zwei_config.hpp
@@ -1,9 +1,16 @@
 #pragma once
-// Defines controlling output
+
+/**
+ * @file: defines configuring the build.
+ */
+
 #if !defined(ZWEI_SLOW)
 #error ZWEI_SLOW={0,1} controls expensive assertions
 #endif
+
+// controls output of features only for internal builds/use
 #if !defined(ZWEI_INTERNAL)
 #error ZWEI_INTERNAL={0,1} marks a build as internal or published
 #endif
+
 #define ZWEI_UNIT_TESTS (ZWEI_SLOW && ZWEI_INTERNAL)
