52b058f78bd5ba92f4be80d5eaf98b7e5ad47fad
Nicolas <nicolas@uucidl.com>
2017-04-08 10:04:35 +0900

    hammer: Add macro syntax

--- src/hammer.hpp
+++ src/hammer.hpp
@@ -1,4 +1,4 @@
-/* @file header file to easily import the hammer library */
+// @file: header file to easily import the hammer library
 extern "C" {
 #include "../builds/include/hammer/glue.h"
 #include "../builds/include/hammer/hammer.h"
