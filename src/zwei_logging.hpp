173925dbd11172006f6b660371429091356d8b99
Nicolas <nicolas@uucidl.com>
2017-04-16 09:19:19 +0200

    logging: Prefix all trace/error prints

--- src/zwei_logging.hpp
+++ src/zwei_logging.hpp
@@ -2,10 +2,6 @@
 
 struct TextOutputGroup;
 
-// TODO(nicolas): traces should feature a timestamp for
-// performance and auditing. i.e. the platform layer should
-// offer a logging service.
-
 void text_output_group_print(int filedesc, TextOutputGroup const &group);
 
 /** traces and clears the group */
@@ -16,4 +12,3 @@ void error(TextOutputGroup *group);
 
 void error_print(char const *message);
 void trace_print(char const *message);
-void trace_print_n(char const *message, size_t message_len);
--- src/zwei_osx_logging.cpp
+++ src/zwei_osx_logging.cpp
@@ -1,92 +1,126 @@
 #include "zwei_logging.hpp"
+
+// TODO(nicolas): trace/error should feature a timestamp for
+// auditing.
+
 #include "zwei_textoutputgroup.hpp"
 
 #include "algos.hpp"
 
 #include <sys/uio.h>
 
-inline void sync_print(int filedesc, char const *message, size_t message_size)
+zw_internal void sync_print(int filedesc,
+                            char const *prefix,
+                            size_t prefix_size,
+                            char const *message,
+                            size_t message_size)
 {
         struct iovec iovecs[] = {
-            {(void *)message, message_size}, {(void *)"\n", 1},
+            {(void *)prefix, prefix_size},
+            {(void *)message, message_size},
+            {(void *)"\n", 1},
         };
         writev(filedesc, iovecs, countof(iovecs));
 }
 
-#define IOVECS_CAPACITY 64
+#define IOVECS_CAPACITY (128 >= IOV_MAX ? IOV_MAX : 128)
 
-void text_output_group_print(int filedesc, TextOutputGroup const &group)
+zw_internal void
+iovecs_write(iovec **iovecs_next_ptr, iovec *iovecs, int filedesc)
 {
-        struct iovec iovecs[IOVECS_CAPACITY];
+        if (*iovecs_next_ptr != iovecs) {
+                writev(filedesc, iovecs, int(*iovecs_next_ptr - iovecs));
+                *iovecs_next_ptr = iovecs;
+        }
+}
+
+zw_internal iovec &iovecs_push(iovec **iovecs_next_ptr,
+                               iovec *iovecs,
+                               iovec *iovecs_last,
+                               int filedesc)
+{
+        if (*iovecs_next_ptr == iovecs_last) {
+                iovecs_write(iovecs_next_ptr, iovecs, filedesc);
+        }
+
+        iovec *entry = *iovecs_next_ptr;
+        ++(*iovecs_next_ptr);
+        return *entry;
+};
+
+zw_internal void text_output_group_print(int filedesc,
+                                         iovec *iovecs,
+                                         iovec *iovecs_last,
+                                         TextOutputGroup const &group)
+{
         auto iovecs_next = iovecs;
-        auto const iovecs_last = iovecs + IOVECS_CAPACITY;
 
-        auto const write_iovecs = [&iovecs_next, &iovecs, filedesc]() {
-                if (iovecs_next != iovecs) {
-                        writev(filedesc, iovecs, int(iovecs_next - iovecs));
-                        iovecs_next = iovecs;
-                }
-        };
-
-        auto next_iovec = [&iovecs_next, iovecs_last,
-                           write_iovecs]() -> iovec & {
-                if (iovecs_next == iovecs_last) {
-                        write_iovecs();
-                }
-
-                iovec *entry = iovecs_next;
-                ++iovecs_next;
-                return *entry;
-        };
-
-        auto first_entry = group.first;
-        auto const last_entry = group.last;
-        algos::for_each(first_entry, last_entry,
-                        [next_iovec](TextOutputGroupEntry const &x) {
-                                auto &iovec = next_iovec();
-                                iovec.iov_base = (void *)(x.first);
-                                iovec.iov_len = x.count;
-                        });
+        algos::for_each(
+            group.first, group.last, [&](TextOutputGroupEntry const &x) {
+                    auto &iovec = iovecs_push(&iovecs_next, iovecs, iovecs_last,
+                                              filedesc);
+                    iovec.iov_base = (void *)(x.first);
+                    iovec.iov_len = x.count;
+            });
 
         if (group.truncated) {
-                auto &iovec = next_iovec();
+                auto &iovec =
+                    iovecs_push(&iovecs_next, iovecs, iovecs_last, filedesc);
                 iovec.iov_base = (void *)"…";
                 iovec.iov_len = sizeof("…") - 1;
         }
 
-        auto &iovec = next_iovec();
+        auto &iovec = iovecs_push(&iovecs_next, iovecs, iovecs_last, filedesc);
         iovec.iov_base = (void *)"\n";
         iovec.iov_len = 1;
-        write_iovecs();
+        iovecs_write(&iovecs_next, iovecs, filedesc);
 }
 
-#undef IOVECS_CAPACITY
+void text_output_group_print(int filedesc, TextOutputGroup const &group)
+{
+        struct iovec iovecs[IOVECS_CAPACITY];
+        auto const iovecs_last = iovecs + IOVECS_CAPACITY;
+        text_output_group_print(filedesc, iovecs, iovecs_last, group);
+}
 
 void error_print(char const *message)
 {
         auto message_size = cstr_len(message);
-        sync_print(2, message, message_size);
+        char const *prefix = "ERROR: ";
+        sync_print(2, prefix, cstr_len(prefix), message, message_size);
 }
 
 void trace_print(char const *message)
 {
         auto message_size = cstr_len(message);
-        sync_print(1, message, message_size);
-}
-
-void trace_print_n(char const *message, size_t message_len)
-{
-        sync_print(1, message, message_len);
+        char const *prefix = "TRACE: ";
+        sync_print(1, prefix, cstr_len(prefix), message, message_size);
 }
 
 void trace(TextOutputGroup *group)
 {
-        text_output_group_print(1, *group);
+        struct iovec iovecs[IOVECS_CAPACITY];
+        auto const iovecs_last = iovecs + IOVECS_CAPACITY;
+
+        char const *prefix = "TRACE: ";
+        iovecs[0].iov_base = (void *)prefix;
+        iovecs[0].iov_len = cstr_len(prefix);
+
+        text_output_group_print(1, iovecs + 1, iovecs_last, *group);
         clear(group);
 }
 
 void error(TextOutputGroup *group)
 {
-        text_output_group_print(2, *group);
+        struct iovec iovecs[IOVECS_CAPACITY];
+        auto const iovecs_last = iovecs + IOVECS_CAPACITY;
+
+        char const *prefix = "ERROR: ";
+        iovecs[0].iov_base = (void *)prefix;
+        iovecs[0].iov_len = cstr_len(prefix);
+
+        text_output_group_print(2, iovecs + 1, iovecs_last, *group);
         clear(group);
 }
+
+#undef IOVECS_CAPACITY
