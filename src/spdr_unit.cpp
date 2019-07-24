cacfa44b1fbd158a024f0c405e117abee6839cdc
Nicolas Léveillé <nicolas@uucidl.com>
2019-07-24 23:59:03 +0200

    Add Linux support.

--- src/spdr_unit.cpp
+++ src/spdr_unit.cpp
@@ -1,5 +1,7 @@
 #if defined(_MSC_VER)
 #include "../modules/uu.spdr/src/spdr_win32_unit.c"
-#else
+#elif defined(__APPLE__)
 #include "../modules/uu.spdr/src/spdr_osx_unit.c"
+#else
+#include "../modules/uu.spdr/src/spdr_linux_unit.c"
 #endif
